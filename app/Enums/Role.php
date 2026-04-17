4ed86d707fe6c48f473a096f3c45b5931627c864
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-17 08:06:23 +0700

    feat: add SUPER_ADMIN role to Role enum

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 enum Role: string
 {
+    case SUPER_ADMIN = 'super_admin';
     case ADMIN = 'admin';
     case CUSTOMER = 'customer';
     case RESTAURANT_OWNER = 'restaurant_owner';
