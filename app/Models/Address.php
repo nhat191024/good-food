0b0da24055834894b5393c908cb54cfa018fead9
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-17 07:57:10 +0700

    feat: update Address model to enhance relationships and documentation

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,13 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int $city_id
+ * @property int $ward_id
+ * @property string $street_address
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Location|null $city
+ * @property-read \App\Models\Customer|null $reviewer
+ * @property-read \App\Models\Location|null $ward
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereCityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereStreetAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereWardId($value)
+ * @mixin \Eloquent
+ */
 #[Fillable(['user_id', 'city_id', 'ward_id', 'street_address'])]
 class Address extends Model
 {
     //Relationships
-    public function user()
+    public function reviewer()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(Customer::class, 'user_id');
     }
 
     public function city()
