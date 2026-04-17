31da65ca7322a746b25d781c6d32748b969a2c60
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-17 08:00:12 +0700

    feat: enhance Location model with additional properties and methods for better functionality

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -6,6 +6,40 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $code
+ * @property string $codename
+ * @property string|null $short_codename
+ * @property string $type
+ * @property string|null $phone_code
+ * @property int|null $parent_id
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Location|null $province
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Location> $wards
+ * @property-read int|null $wards_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCodename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location wherePhoneCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereShortCodename($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location withTrashed(bool $withTrashed = true)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Location withoutTrashed()
+ * @mixin \Eloquent
+ */
 #[Fillable(['name', 'code', 'codename', 'short_codename', 'type', 'phone_code', 'parent_id'])]
 class Location extends Model
 {
