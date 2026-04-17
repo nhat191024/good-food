c130ba8b669de246a7e53cbb5bb35a2401634578
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-04-17 12:51:49 +0700

    feat: add subcategories relationship to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -48,6 +48,11 @@ class Category extends Model
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
+    public function subcategories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
     public function restaurant()
     {
         return $this->belongsTo(Restaurant::class);
